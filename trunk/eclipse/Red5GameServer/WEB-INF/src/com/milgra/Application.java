387c4ac0d6b6a796b173c168e446780758814bf9
ezra.ezz <ezra.ezz@c09a8362-2635-11df-b7b8-dd6b17a02e3e>
2010-03-26 20:22:26 +0000

    Trying to add a hostlist on the server in Java but i'm getting errors, and I don't understand why. Probably doing something wrong, not sure what. Red5 quirck or do I just suck at Java?
    Added an RTMP Client Console application that is handy for testing out SharedObjects and stuff.

--- trunk/eclipse/Red5GameServer/WEB-INF/src/com/milgra/Application.java
+++ trunk/eclipse/Red5GameServer/WEB-INF/src/com/milgra/Application.java
@@ -1,20 +1,25 @@
 package com.milgra;
 
+import java.util.ArrayList;
+
 import org.red5.server.adapter.ApplicationAdapter;
 import org.red5.server.api.IConnection;
 import org.red5.server.api.IScope;
 
 public class Application extends ApplicationAdapter
 {
+	private ArrayList<String> Hosts;
+
     public Boolean appStart ( )
     {
+    	Hosts = new ArrayList<String>();
     	return true;
     }
     public void appStop ( )
     {
 
     }
-    public boolean appConnect( IConnection conn, Object[] params )
+    public boolean appConnect( IConnection conn, IScope scope, Object[] params )
     {
     	//boolean accept = (Boolean)params[0];
 
@@ -28,5 +33,14 @@ public class Application extends ApplicationAdapter
     public void appDisconnect( IConnection conn)
     {
        super.appDisconnect(conn);
+    }
+    public ArrayList<String> getHosts()
+    {
+    	return Hosts;
+    }
+    public Boolean HostGame(String name)
+    {
+    	Hosts.add("name");
+    	return true;
     }
 }
