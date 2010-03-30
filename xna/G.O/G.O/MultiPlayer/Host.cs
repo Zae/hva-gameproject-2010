8ecb1b5b73307cc185a00ea7b37c82084cc754dc
Ezra Pool <Zae@users.noreply.github.com>
2010-03-30 10:44:55 +0000

    Struct voor Host (hostlijst)

---
+++ xna/G.O/G.O/MultiPlayer/Host.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ION.MultiPlayer
+{
+    struct Host
+    {
+        String hostname;
+        Object scope;
+    }
+}
