eb522a17754274307e335c5fe0e0d8c86d83a6c8
ezra.ezz <ezra.ezz@c09a8362-2635-11df-b7b8-dd6b17a02e3e>
2010-04-08 23:03:52 +0000

    Re-enabled the network code, it should compile now. Disabled the little part that actually sends the grid every update because that slows everything down a loooot and it's not what we're actually going to use later, just for testing and stuff...
    Added a Tile serializer and deserializer to the Serializer class, we can't deserialize to a tile because there is too much info lost on serializing. It basically converts the map's charges to a multidimensional floatarray that is encoded in a bytearray.

--- trunk/xna/G.O/G.O/MultiPlayer/Serialization.cs
+++ trunk/xna/G.O/G.O/MultiPlayer/Serialization.cs
@@ -17,16 +17,67 @@ namespace ION
         {
             throw new NotImplementedException();
         }
-        public static Byte[] Serialize(Tile[,] input)
+        public static Byte[] Serialize(Tile[,] input, int width, int height)
         {
-            throw new NotImplementedException();
+            System.IO.MemoryStream stream = new System.IO.MemoryStream();
+            System.IO.BinaryWriter writer = new System.IO.BinaryWriter(stream);
+
+            writer.Write(width);
+            writer.Write(height);
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    if (input[i, j] is ResourceTile)
+                    {
+                        ResourceTile t = (ResourceTile)input[i, j];
+                        writer.Write(t.charge);
+                    }
+                    else
+                    {
+                        writer.Write(0f);
+                    }
+                }
+            }
+
+            byte[] byteArray = new byte[stream.Length];
+            stream.Seek(0, System.IO.SeekOrigin.Begin);
+
+            stream.Read(byteArray, 0, (int)stream.Length); // Make sure stream isn't too long, might want to check Length vs. int.MaxValue
+            stream.Close();
+
+            return byteArray;
         }
         #endregion
 
         #region Deserialize
-        public static Tile[,] Deserialize(Byte[] input)
+        public static float[,] Deserialize(Byte[] input)
         {
-            throw new NotImplementedException();
+            System.IO.MemoryStream stream = new System.IO.MemoryStream();
+            stream.Write(input, 0, input.Length); // Make sure stream isn't too long, might want to check Length vs. int.MaxValue
+            stream.Seek(0, System.IO.SeekOrigin.Begin);
+            System.IO.BinaryReader reader = new System.IO.BinaryReader(stream);
+
+            int width = reader.ReadInt32();
+            int height = reader.ReadInt32();
+
+            float[,] result = new float[width, height];
+
+            int counter = 0;
+            while (stream.Position < stream.Length)
+            {
+                /** Calculate the indexes **/
+                int i = (int)counter/width;
+                int j = counter % height;
+                result[i, j] = reader.ReadSingle();
+                counter++;
+            }
+
+            reader.Close();
+            stream.Close();
+            
+            return result;
         }
         #endregion
     }
