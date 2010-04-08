ddb96aa45d187d9752b0cc916382c1d1de617706
ezra.ezz <ezra.ezz@c09a8362-2635-11df-b7b8-dd6b17a02e3e>
2010-04-08 14:20:03 +0000

    More network serialize changes and stuff..
    It WILL send the array now, started a new Serializer in Serializer.cs to standarize the serialization and deserialization of object and primitives.

---
+++ trunk/xna/G.O/G.O/MultiPlayer/Serialization.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ION
+{
+    public class Serializer
+    {
+        #region Serialize
+        public static Byte[] Serialize(String[] input)
+        {
+            throw new NotImplementedException();
+        }
+        public static Byte[] Serialize(float[] input)
+        {
+            throw new NotImplementedException();
+        }
+        public static Byte[] Serialize(float[,] input)
+        {
+            throw new NotImplementedException();
+        }
+        public static Byte[] Serialize(Tile[,] input)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Deserialize
+        public static Tile[,] Deserialize(Byte[] input)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
