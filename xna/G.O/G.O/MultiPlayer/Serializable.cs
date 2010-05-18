8ca2c000490ad79a5be34870f831afb2e8321d90
Ezra Pool <Zae@users.noreply.github.com>
2010-05-18 09:36:32 +0000

    Commented Serializable

--- xna/G.O/G.O/MultiPlayer/Serializable.cs
+++ xna/G.O/G.O/MultiPlayer/Serializable.cs
@@ -8,7 +8,15 @@ namespace ION
     /// <seealso cref="Serializer"/>
     public interface Serializable
     {
+        /// <summary>
+        /// Serialize your custom object into a bytearray through the <see cref="Serializer">Serializer</see>
+        /// </summary>
+        /// <returns>A Memorystream with the to be serialized data.</returns>
         MemoryStream Serialize();
+        /// <summary>
+        /// Deserialize your custom object into a bytearray through the <see cref="Serializer">Serializer</see>
+        /// </summary>
+        /// <param name="inData">A Memorystream with the to be deserialized data.</param>
         void Deserialize(MemoryStream inData);
     }
 }
