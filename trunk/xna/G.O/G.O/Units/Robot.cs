bb7d6c8fb0b186cfa01c19fa9c562ef1ff81aae6
michiel.van.den.anker <michiel.van.den.anker@c09a8362-2635-11df-b7b8-dd6b17a02e3e>
2010-05-25 02:25:13 +0000

    You now see units getting hit. Need to draw the bullet impact randomly around the unit.

--- trunk/xna/G.O/G.O/Units/Robot.cs
+++ trunk/xna/G.O/G.O/Units/Robot.cs
@@ -52,54 +52,35 @@ namespace ION
                
             
 
-            drawUnderFireAnimation();
+            drawUnderFireAnimation(x,y);
 
         }
 
-        private void drawUnderFireAnimation()
+        private void drawUnderFireAnimation(float x, float y)
         {
-            //if (underFire)
-            //{
-            //    //animation test code
-            //    UnderFireCounter++;
+            if (underFire)
+            {
+                //animation test code
+                UnderFireCounter++;
 
-            //    if (UnderFireCounter > 0)
-            //    {
-            //        UnderFireFrame = 0;
-            //    }
-            //    if (UnderFireCounter > 4)
-            //    {
-            //        FiringFrame = 1;
-            //    }
-            //    if (FiringCounter > 10)
-            //    {
-            //        FiringFrame = 2;
-            //    }
-            //    if (FiringCounter > 14)
-            //    {
-            //        FiringCounter = 0;
-            //    }
+                if (UnderFireCounter > 0)
+                {
+                    UnderFireFrame = 0;
+                }
+                if (UnderFireCounter > 14)
+                {
+                    UnderFireFrame = 1;            
+                }
+                if (UnderFireCounter > 25)
+                {
+                    UnderFireCounter = 0;
+                    underFire = false;
+                    return;
+                }
 
-            //    if (selected && FiringFrame < 2)
-            //    {
-            //        //do not remove
-            //        //ION.spriteBatch.Draw(Images.getUnitImage(owner, (int)facing, selected), new Rectangle((int)(((pos.X - ION.halfWidth) * (scale / 15.0f)) + ION.halfWidth + (x)), (int)(((pos.Y) * (scale / 15.0f)) + (y) + (baseHalfHeight * 2)), (int)(baseHalfWidth * 2), (int)(baseHalfHeight * 4)), Color.White);
-
-            //        ION.spriteBatch.Draw(Images.unit_selected_shooting[owner - 1, (int)facing, FiringFrame], new Rectangle((int)(((pos.X - ION.halfWidth) * (scale / 15.0f)) + ION.halfWidth + (x)), (int)(((pos.Y) * (scale / 15.0f)) + (y) + (baseHalfHeight * 2)), (int)(baseHalfWidth * 2), (int)(baseHalfHeight * 4)), Color.White);
-            //    }
-            //    else if (!selected && FiringFrame < 2)
-            //    {
-            //        ION.spriteBatch.Draw(Images.unit_shooting[owner - 1, (int)facing, FiringFrame], new Rectangle((int)(((pos.X - ION.halfWidth) * (scale / 15.0f)) + ION.halfWidth + (x)), (int)(((pos.Y) * (scale / 15.0f)) + (y) + (baseHalfHeight * 2)), (int)(baseHalfWidth * 2), (int)(baseHalfHeight * 4)), Color.White);
-            //    }
-
-            //    if (FiringFrame >= 2)
-            //    {
-            //        //do not remove
-            //        ION.spriteBatch.Draw(Images.getUnitImage(owner, (int)facing, selected), new Rectangle((int)(((pos.X - ION.halfWidth) * (scale / 15.0f)) + ION.halfWidth + (x)), (int)(((pos.Y) * (scale / 15.0f)) + (y) + (baseHalfHeight * 2)), (int)(baseHalfWidth * 2), (int)(baseHalfHeight * 4)), Color.White);
-
-            //        //ION.spriteBatch.Draw(Images.unit_selected_shooting[owner - 1, (int)facing, FiringFrame], new Rectangle((int)(((pos.X - ION.halfWidth) * (scale / 15.0f)) + ION.halfWidth + (x)), (int)(((pos.Y) * (scale / 15.0f)) + (y) + (baseHalfHeight * 2)), (int)(baseHalfWidth * 2), (int)(baseHalfHeight * 4)), Color.White);
-            //    }
-            //}
+                ION.spriteBatch.Draw(Images.bulletImpact[UnderFireFrame], new Rectangle((int)(((pos.X - ION.halfWidth) * (scale / 15.0f)) + ION.halfWidth + (x)), (int)(((pos.Y) * (scale / 15.0f)) + (y) + (baseHalfHeight * 0.5)), (int)(baseHalfWidth * 0.5), (int)(baseHalfHeight * 4)), Color.White);
+                
+            }
         }
 
         private bool drawFiringAnimation(float x, float y)
