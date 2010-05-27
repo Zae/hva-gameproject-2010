094bce911c40da94786a0a26f33bc6ad4bbe1309
michiel.van.den.anker <michiel.van.den.anker@c09a8362-2635-11df-b7b8-dd6b17a02e3e>
2010-05-27 09:56:14 +0000

    Can add empty tiles to the map.

---
+++ trunk/xna/G.O/G.O/Grid/VoidTile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ION
+{
+    class VoidTile : Tile
+    {
+
+        public VoidTile(int indexX, int indexY)
+        {
+            accessable = false;
+
+            this.indexX = indexX;
+            this.indexY = indexY;
+        }
+
+        public override void drawDebug(float translationX, float translationY)
+        {
+        }
+
+        public override void draw(float translationX, float translationY)
+        {          
+        }
+
+        public override void update()
+        {
+        }
+    }
+}
