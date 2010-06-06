d67373e7bf199456108b3425ed29eeea06974bb1
Ezra Pool <Zae@users.noreply.github.com>
2010-06-06 12:05:18 +0000

    Using GUIManager for StateTitle and StateMP

--- xna/G.O/G.O/State/StateMP.cs
+++ xna/G.O/G.O/State/StateMP.cs
@@ -8,6 +8,7 @@ using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 using System.Net;
 using System.IO;
+using WindowSystem;
 
 
 
@@ -15,62 +16,88 @@ namespace ION
 {
     class StateMP : State
     {
-        public enum SELECTION
-        {
-            HOST = 1,
-            JOIN,
-            BACK
+        private List<UIComponent> ComponentList;
 
-        }
-        public SELECTION selection = SELECTION.HOST;
-
-
-        private Rectangle newGameButton;
-        private Rectangle mpButton;
-        private Rectangle quitButton;
-        private Rectangle optionsButton;
+        private TextButton newGameButton;
+        private TextButton mpButton;
+        private TextButton quitButton;
+        private TextButton optionsButton;
+        private TextButton hostButton;
+        private TextButton joinButton;
+        private TextButton backButton;
+        
         private Rectangle background_overlay;
         private Rectangle Logo;
         private Rectangle background_starfield;
-        //
-        private Rectangle hostButton;
-        private Rectangle joinButton;
-        private Rectangle backButton;
-
-        private bool mousePressed = false;
-        public bool upPressed = false;
-        public bool downPressed = false;
-        public bool enterPressed = false;
-
 
         public StateMP()
         {
-            newGameButton = new Rectangle(125, 125, Images.buttonNewGame.Width, Images.buttonNewGame.Height);
-            mpButton = new Rectangle(125, 200, Images.buttonNewGame.Width, Images.buttonNewGame.Height);
-            optionsButton = new Rectangle(125, 275, Images.buttonOptions.Width, Images.buttonOptions.Height);
-            quitButton = new Rectangle(125, 350, Images.buttonNewGame.Width, Images.buttonNewGame.Height);
+            ComponentList = new List<UIComponent>();
+
+            newGameButton = new TextButton(ION.instance, ION.instance.gui);
+            newGameButton.X = 125; newGameButton.Y = 125;
+            newGameButton.Text = "New Game";
+            newGameButton.Enabled = false;
+            mpButton = new TextButton(ION.instance, ION.instance.gui);
+            mpButton.X = 125; mpButton.Y = 200;
+            mpButton.Text = "Multiplayer";
+            optionsButton = new TextButton(ION.instance, ION.instance.gui);
+            optionsButton.X = 125; optionsButton.Y = 275;
+            optionsButton.Text = "Options";
+            quitButton = new TextButton(ION.instance, ION.instance.gui);
+            quitButton.X = 125; quitButton.Y = 350;
+            quitButton.Text = "Quit";
+
+            hostButton = new TextButton(ION.instance, ION.instance.gui);
+            hostButton.Click += new ClickHandler(hostButton_Click);
+            hostButton.X = 375; hostButton.Y = 200;
+            hostButton.Text = "Host";
+            joinButton = new TextButton(ION.instance, ION.instance.gui);
+            joinButton.Click += new ClickHandler(joinButton_Click);
+            joinButton.X = 375; joinButton.Y = 275;
+            joinButton.Text = "Join";
+            backButton = new TextButton(ION.instance, ION.instance.gui);
+            backButton.Click += new ClickHandler(backButton_Click);
+            backButton.X = 375; backButton.Y = 350;
+            backButton.Text = "Back";
+
+            ComponentList.Add(newGameButton);
+            ComponentList.Add(mpButton);
+            ComponentList.Add(optionsButton);
+            ComponentList.Add(quitButton);
+            ComponentList.Add(hostButton);
+            ComponentList.Add(joinButton);
+            ComponentList.Add(backButton);
+
             background_overlay = new Rectangle(ION.width - Images.background_overlay.Width, 0, Images.background_overlay.Width, Images.background_overlay.Height);
             Logo = new Rectangle(ION.width / 100 * 10, ION.height - ION.height / 100 * 7 - Images.Logo.Height, Images.Logo.Width, Images.Logo.Height);
             background_starfield = new Rectangle(0, 0, Images.background_starfield.Width, Images.background_starfield.Height);
-            //
-            hostButton = new Rectangle(375, 200, Images.buttonNewGame.Width, Images.buttonNewGame.Height);
-            joinButton = new Rectangle(375, 275, Images.buttonNewGame.Width, Images.buttonNewGame.Height);
-            backButton = new Rectangle(375, 350, Images.buttonNewGame.Width, Images.buttonNewGame.Height);
 
             if (ION.instance.serverConnection == null)
             {
                 ION.instance.serverConnection = new ServerConnection();
             }
+        }
 
-         
+        void backButton_Click(UIComponent sender)
+        {
+            ION.get().setState(new StateTitle());
+        }
+
+        void joinButton_Click(UIComponent sender)
+        {
+            ION.get().setState(new StateJoin());
+        }
+
+        void hostButton_Click(UIComponent sender)
+        {
+            ION.get().setState(new StateHost());
         }
 
         public override void draw()
         {
-            
             ION.get().GraphicsDevice.Clear(Color.Black);
             ION.spriteBatch.Begin();
-            //
             //logo
             Double w = Math.Ceiling((Double)ION.width / (Double)Images.background_overlay.Width);
             Double h = Math.Ceiling((Double)ION.height / (Double)Images.background_overlay.Height);
@@ -83,44 +110,6 @@ namespace ION
             }
             ION.spriteBatch.Draw(Images.background_overlay, background_overlay, Color.White);
             ION.spriteBatch.Draw(Images.Logo, Logo, Color.White);
-            //
-            ION.spriteBatch.Draw(Images.buttonNewGame, newGameButton, Color.Gray);
-            ION.spriteBatch.Draw(Images.buttonMP, mpButton, Color.Gray);
-            ION.spriteBatch.Draw(Images.buttonOptions, optionsButton, Color.Gray);
-            ION.spriteBatch.Draw(Images.buttonQuit, quitButton, Color.Gray);
-            //
-            if (selection == SELECTION.HOST)
-            {
-                //Draw highlighted
-                ION.spriteBatch.Draw(Images.buttonHostF, hostButton, Color.White);
-            }
-            else
-            {
-                //Draw normally
-                ION.spriteBatch.Draw(Images.buttonHost, hostButton, Color.White);
-            }
-            if (selection == SELECTION.JOIN)
-            {
-                //Draw highlighted
-                ION.spriteBatch.Draw(Images.buttonJoinF, joinButton, Color.White);
-            }
-            else
-            {
-                //Draw normally
-                ION.spriteBatch.Draw(Images.buttonJoin, joinButton, Color.White);
-            }
-
-
-            if (selection == SELECTION.BACK)
-            {
-                //Draw highlighted
-                ION.spriteBatch.Draw(Images.buttonBackF, backButton, Color.White);
-            }
-            else
-            {
-                //Draw normally
-                ION.spriteBatch.Draw(Images.buttonBack, backButton, Color.White);
-            }
 
             ION.spriteBatch.End();
         }
@@ -128,146 +117,28 @@ namespace ION
 
         public override void update(int ellapsed)
         {
-            //mouse handling
-            MouseState mouseState = Mouse.GetState();
-            if (mouseState.LeftButton == ButtonState.Pressed)
-            {
-                mousePressed = true;
-            }
-
-            if (mouseIn(mouseState.X, mouseState.Y, hostButton))
-            {
-                selection= SELECTION.HOST;
-                if (mouseState.LeftButton == ButtonState.Released && mousePressed == true)
-                {
-                    makeSelection();
-                    mousePressed = false;
-                }
-
-            }
-            if (mouseIn(mouseState.X, mouseState.Y,joinButton))
-            {
-                selection = SELECTION.JOIN;
-                if (mouseState.LeftButton == ButtonState.Released && mousePressed == true)
-                {
-                    makeSelection();
-                    mousePressed = false;
-                }
-
-            }
-            if (mouseIn(mouseState.X, mouseState.Y, backButton))
-            {
-                selection = SELECTION.BACK;
-                if (mouseState.LeftButton == ButtonState.Released && mousePressed == true)
-                {
-                    makeSelection();
-                    mousePressed = false;
-                }
-
-            }  
-
-            //Keyboard handling
-            KeyboardState keyState = Keyboard.GetState();
-
-            if (keyState.IsKeyDown(Keys.Up) && !upPressed)
-            {
-                selectionUp();
-                upPressed = true;
-            }
-            else if (keyState.IsKeyUp(Keys.Up) && upPressed)
-            {
-                upPressed = false;
-            }
-
-            if (keyState.IsKeyDown(Keys.Down) && !downPressed)
-            {
-                selectionDown();
-                downPressed = true;
-            }
-            else if (keyState.IsKeyUp(Keys.Down) && downPressed)
-            {
-                downPressed = false;
-            }
-
-            if (keyState.IsKeyDown(Keys.Enter))
-            {
-                enterPressed = true;
-            }
-
-            if (keyState.IsKeyUp(Keys.Enter) && enterPressed == true)
-            {
-                enterPressed = false;
-                makeSelection();
-            }          
-        }
-
-        private void makeSelection()
-        {
-            if (selection == SELECTION.HOST)
-            {
-                StateHost st = new StateHost();
-
-                ION.get().setState(st);
-            }
-            else if (selection == SELECTION.BACK)
-            {
-                StateTitle st = new StateTitle();
          
-                ION.get().setState(st);
-            }
-           
-            else if (selection == SELECTION.JOIN)
-            {
-                //Console.WriteLine("join");
-                //"http://landconquerer.appspot.com/landconquerer
-               // WebRequest req = WebRequest.Create("http://google.com");
-                //WebResponse response = req.GetResponse();
-                //StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.ASCII);
-                StateJoin st = new StateJoin();
-
-                ION.get().setState(st);
-                //Console.WriteLine(sr.ReadToEnd());
-            }
-        }
-
-        private void selectionUp()
-        {
-
-            selection--;
-            if (selection < SELECTION.HOST)
-            {
-                Console.WriteLine("if sel: " + (int)selection);
-                selection = (SELECTION)Enum.GetNames(typeof(SELECTION)).Length;
-            }
-            Console.WriteLine("sel: " + (int)selection);
-        }
-
-        private void selectionDown()
-        {
-            selection++;
-            if ((int)selection > Enum.GetNames(typeof(SELECTION)).Length)
-            {
-                Console.WriteLine("if sel: " + (int)selection);
-                selection = SELECTION.HOST;
-            }
-            Console.WriteLine("sel: " + (int)selection);
-
-
         }
 
         public override void focusGained()
         {
-            ION.get().IsMouseVisible = true;
+            foreach (UIComponent uicomponent in ComponentList)
+            {
+                ION.instance.gui.Add(uicomponent);
+            }
+            //
             MediaPlayer.Play(Sounds.titleSong);
             MediaPlayer.IsRepeating = true;
         }
 
-
         public override void focusLost()
         {
-            ION.get().IsMouseVisible = false;
+            foreach (UIComponent uicomponent in ComponentList)
+            {
+                ION.instance.gui.Remove(uicomponent);
+            }
+            //
             MediaPlayer.Stop();
         }
-
     }
 }
